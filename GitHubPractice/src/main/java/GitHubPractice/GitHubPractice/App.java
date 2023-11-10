9876b236d5c0e0390653d45565e89db0c9e96e59
Lakshmi0339 <laxmijntu1989@gmail.com>
2023-11-10 10:45:32 +0530

    Commit on 10 nov

---
+++ GitHubPractice/src/main/java/GitHubPractice/GitHubPractice/App.java
@@ -0,0 +1,13 @@
+package GitHubPractice.GitHubPractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
